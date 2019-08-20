741e70f625e3751c7b599ca849d0ad8d09100e91
illusionist <nikhil.shrestha1995@gmail.com>
2019-08-20 13:47:02 +0545

    add tabs to our app

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import { Navigation } from 'react-native-navigation';
 
 import AuthScreen from './src/screens/Auth/Auth';
+import SharePlace from './src/screens/SharePlace/SharePlace';
+import FindPlace from './src/screens/FindPlace/FindPlace';
 
 // Register Screens
 Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
+Navigation.registerComponent(
+  'awesome-places.SharePlaceScreen',
+  () => SharePlace
+);
+Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlace);
 
 // Start App
 Navigation.events().registerAppLaunchedListener(() => {
@@ -13,17 +20,17 @@ Navigation.events().registerAppLaunchedListener(() => {
         children: [
           {
             component: {
-              name: 'awesome-places.AuthScreen'
+              name: 'awesome-places.AuthScreen',
+              options: {
+                topBar: {
+                  title: {
+                    text: 'Login'
+                  }
+                }
+              }
             }
           }
-        ],
-        options: {
-          topBar: {
-            title: {
-              text: 'Welcome screen'
-            }
-          }
-        }
+        ]
       }
     }
   });
---
+++ src/screens/MainTabs/startMainTabs.js
@@ -0,0 +1,66 @@
+import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const startTabs = () => {
+  Promise.all([
+    Icon.getImageSource('md-map', 30),
+    Icon.getImageSource('ios-share-alt', 30)
+  ]).then(sources => {
+    Navigation.setRoot({
+      root: {
+        bottomTabs: {
+          children: [
+            {
+              stack: {
+                children: [
+                  {
+                    component: {
+                      name: 'awesome-places.FindPlaceScreen',
+                      options: {
+                        topBar: {
+                          title: {
+                            text: 'Find Place'
+                          }
+                        },
+                        bottomTab: {
+                          text: 'Find Place',
+                          icon: sources[0],
+                          testID: 'FIND_TAB'
+                        }
+                      }
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              stack: {
+                children: [
+                  {
+                    component: {
+                      name: 'awesome-places.SharePlaceScreen',
+                      options: {
+                        topBar: {
+                          title: {
+                            text: 'Share Place'
+                          }
+                        },
+                        bottomTab: {
+                          text: 'Share Place',
+                          icon: sources[1],
+                          testID: 'SHARE_TAB'
+                        }
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    });
+  });
+};
+
+export default startTabs;

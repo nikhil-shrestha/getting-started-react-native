00a42115ae3c42ae847d1adf3a7fa44ee6a35907
illusionist <nikhil.shrestha1995@gmail.com>
2019-08-21 16:40:48 +0545

    adding custom button for loading

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { Navigation } from 'react-native-navigation';
 
 import PlaceList from '../../components/PlaceList/PlaceList';
 
 class FindPlaceScreen extends Component {
+  state = {
+    placesLoaded: false
+  };
+
   constructor(props) {
     super(props);
     this.isSideDrawerVisible = false;
@@ -47,17 +51,55 @@ class FindPlaceScreen extends Component {
     });
   };
 
+  placesSearchHandler = () => {
+    this.setState({
+      placesLoaded: true
+    });
+  };
+
   render() {
-    return (
-      <View>
+    let content = (
+      <TouchableOpacity onPress={this.placesSearchHandler}>
+        <View style={styles.searchButton}>
+          <Text style={styles.searchButtonText}>Find Places</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
+    if (this.state.placesLoaded) {
+      content = (
         <PlaceList
           places={this.props.places}
           onItemSelected={this.itemSelectedHandler}
         />
+      );
+    }
+    return (
+      <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+        {content}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  listContainer: {},
+  searchButton: {
+    borderColor: 'orange',
+    borderWidth: 3,
+    borderRadius: 50,
+    padding: 20
+  },
+  searchButtonText: {
+    color: 'orange',
+    fontWeight: 'bold'
+  }
+});
 
 const mapStateToProps = state => ({
   places: state.places.places

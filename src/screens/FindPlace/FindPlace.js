60d8e2fd805283825c428176a231776bb33519b8
illusionist <nikhil.shrestha1995@gmail.com>
2019-08-21 17:01:26 +0545

    updated animation for list

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -14,7 +14,8 @@ import PlaceList from '../../components/PlaceList/PlaceList';
 class FindPlaceScreen extends Component {
   state = {
     placesLoaded: false,
-    removeAnim: new Animated.Value(1)
+    removeAnim: new Animated.Value(1),
+    placesAnim: new Animated.Value(0)
   };
 
   constructor(props) {
@@ -58,15 +59,25 @@ class FindPlaceScreen extends Component {
     });
   };
 
+  placesLoadedHandler = () => {
+    Animated.timing(this.state.placesAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  };
+
   placesSearchHandler = () => {
-    // this.setState({
-    //   placesLoaded: true
-    // });
     Animated.timing(this.state.removeAnim, {
       toValue: 0,
       duration: 500,
       useNativeDriver: true
-    }).start();
+    }).start(() => {
+      this.setState({
+        placesLoaded: true
+      });
+      this.placesLoadedHandler();
+    });
   };
 
   render() {
@@ -94,10 +105,16 @@ class FindPlaceScreen extends Component {
 
     if (this.state.placesLoaded) {
       content = (
-        <PlaceList
-          places={this.props.places}
-          onItemSelected={this.itemSelectedHandler}
-        />
+        <Animated.View
+          style={{
+            opacity: this.state.placesAnim
+          }}
+        >
+          <PlaceList
+            places={this.props.places}
+            onItemSelected={this.itemSelectedHandler}
+          />
+        </Animated.View>
       );
     }
     return (

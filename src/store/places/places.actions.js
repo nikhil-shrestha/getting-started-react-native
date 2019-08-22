f663171bb0a945bc97dff872b43e480c47d11772
illusionist <nikhil.shrestha1995@gmail.com>
2019-08-22 12:29:20 +0545

    uploaing information to database

--- src/store/places/places.actions.js
+++ src/store/places/places.actions.js
@@ -2,19 +2,36 @@ import * as actionTypes from './places.types';
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    const placeData = {
-      name: placeName,
-      location
-    };
-    fetch('https://rn-course-1566437520068.firebaseio.com/places.json', {
-      method: 'POST',
-      body: JSON.stringify(placeData)
-    })
+    fetch(
+      'https://us-central1-rn-course-1566437520068.cloudfunctions.net/storeImage',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          image: image.base64
+        })
+      }
+    )
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placeData = {
+          name: placeName,
+          location,
+          image: parsedRes.imageUrl
+        };
+        return fetch(
+          'https://rn-course-1566437520068.firebaseio.com/places.json',
+          {
+            method: 'POST',
+            body: JSON.stringify(placeData)
+          }
+        );
+      })
+      .catch(err => console.log(err))
       .then(res => res.json())
       .then(parsedRes => {
         console.log(parsedRes);
-      })
-      .catch(err => console.log(err));
+      });
   };
 };
 

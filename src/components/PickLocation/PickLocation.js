10ed4b6c7e75d7d1c01e91ba2373b2825d6f15ce
illusionist <nikhil.shrestha1995@gmail.com>
2019-08-22 07:58:50 +0545

    locating user

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -32,6 +32,26 @@ export default class PickLocation extends Component {
     }));
   };
 
+  getLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        const coordsEvent = {
+          nativeEvent: {
+            coordinate: {
+              latitude: pos.coords.latitude,
+              longitude: pos.coords.longitude
+            }
+          }
+        };
+        this.pickLocationHandler(coordsEvent);
+      },
+      err => {
+        console.log(err);
+        alert('Fetching the Position failed, Please pick one manually!');
+      }
+    );
+  };
+
   render() {
     let marker = null;
 
@@ -51,7 +71,7 @@ export default class PickLocation extends Component {
           {marker}
         </MapView>
         <View style={styles.button}>
-          <Button title="Locate Me" onPress={() => alert('Pick Location')} />
+          <Button title="Locate Me" onPress={this.getLocationHandler} />
         </View>
       </View>
     );
